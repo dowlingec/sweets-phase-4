eb312d13615b546047b9fdbfdb62d56a59323e25
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-26 14:00:29 -0400

    rescue clause written

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,16 +1,22 @@
 class VendorsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
     def index
         vendors = Vendor.all
         render json: vendors
     end
 
     def show
-        vendor = Vendor.find_by(id: params[:id])
-        render json: vendor
+        vendor = Vendor.find_by!(id: params[:id])
+        render json: vendor, include: [:sweets]
     end
 
     private
     def vendor_params
         params.permit(:id, :name)
+    end
+
+    def render_record_not_found
+        render json: { "error": "Vendor not found" }
     end
 end
