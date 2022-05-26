2bdf788a7676d2c801fe3d5bced57ef4e459886a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-26 14:05:42 -0400

    sweets index show tested w rescue clause

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,2 +1,22 @@
 class SweetsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+    def index
+        sweets = Sweet.all
+        render json: sweets
+    end
+
+    def show
+        sweet = Sweet.find_by!(id: params[:id])
+        render json: sweet
+    end
+
+    private
+    def sweet_params
+        params.permit(:id, :name)
+    end
+
+    def render_record_not_found
+        render json: { "error": "Sweet not found" }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :vendor_sweets
   resources :vendors, only: [:index, :show]
-  resources :sweets
+  resources :sweets, only:[:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
