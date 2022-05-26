23ebaa62f925f44dc014f344b03e6cc2a343f20d
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-26 14:22:10 -0400

    delete vendor sweet

--- app/controllers/vendor_sweets_controller.rb
+++ app/controllers/vendor_sweets_controller.rb
@@ -1,2 +1,27 @@
 class VendorSweetsController < ApplicationController
+# rescue_from ActiveRecord::RecordInvalid, with: :
+    # DELETE ME!!!
+    def index
+        vendorsweets = VendorSweet.all
+        render json: vendorsweets
+    end
+    def create
+        vendorsweet = VendorSweet.create!(vendor_sweets_params)
+        render json: vendorsweet
+    end
+
+    def destroy
+        vendorsweet = VendorSweet.find_by!(id: params[:id])
+        vendorsweet.destroy
+        head :no_content
+    end
+
+    private
+    def vendor_sweets_params
+        params.permit(:price, :vendor_id, :sweet_id)
+    end
+
+    # def invalid_record
+    #     render json: { "errors": ["validation errors"]}
+    # end
 end
